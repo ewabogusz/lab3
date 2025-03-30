02fed67959b4869d1a837eb2862ca039b6263373
Ewa Boguszewska <ewa.m.boguszewska@gmail.com>
2025-03-30 11:17:30 +0200

    init

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("lab3");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("lab3");
+using System;
+namespace lab3
+{
+    class program
+    {
+        static void main()
+        {
+
+        }
+    }
+    
+}
