8f089023c5d960ba404d90731f37a9301d90ea23
Ewa Boguszewska <ewa.m.boguszewska@gmail.com>
2025-03-30 12:43:34 +0200

    init

--- lab3/lab3/lab3.cs
+++ lab3/lab3/lab3.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab3
 {
     class Program
     {
-        //klasa próbna
+        // Klasa próbna na zajęciach
         public class Human
         {
             public string FirstName { get; set; }
@@ -18,13 +19,51 @@ namespace Lab3
 
         static void Main()
         {
-            Console.WriteLine("klasa probna");
+            Console.WriteLine("KLASA PRÓBNA\n");
             Human human = new Human
             {
                 FirstName = "Ewa",
                 LastName = "Boguszewska"
             };
             human.SayHello();
+
+            Console.WriteLine("KOLEKCJE\n");
+            Console.WriteLine("Zadanie 1\n");
+
+            string[] colors = { "blue", "green", "yellow", "red", "orange" };
+            Console.WriteLine($"Mój pierwszy kolor w tablicy to: {colors[0]}\n");
+            Console.WriteLine($"Mój ostatni kolor w tablicy to: {colors[colors.Length - 1]}\n");
+
+            Console.WriteLine("Zadanie 2\n");
+            int[] tab = { 1, 2, 3, 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine("Wypisanie liczb z tablicy:\n");
+
+            Console.WriteLine("Pętla for");
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine($"Liczba: {tab[i]}");
+            }
+
+            Console.WriteLine("\nPętla foreach");
+            foreach (int number in tab)
+            {
+                Console.WriteLine($"Liczba: {number}");
+            }
+
+            Console.WriteLine("\nPętla while");
+            int index = 0;
+            while (index < tab.Length)
+            {
+                Console.WriteLine($"Liczba: {tab[index]}");
+                index++;
+            }
+
+            Console.WriteLine("\nZadanie 3\n");
+            List<string> fruits = new List<string> { "malina", "jablko", "truskawka", "gruszka","mango","jagoda" };
+            Console.WriteLine(string.Join(", ", fruits) + "\n");
+            fruits.RemoveAt(0);
+            fruits.RemoveAt(fruits.Count - 1);
+            Console.WriteLine(string.Join(", ", fruits) + "\n");
         }
     }
 }
